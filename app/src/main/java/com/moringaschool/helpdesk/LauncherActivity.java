bf3accd2e03d935def581556aa61a6978b013299
ericwafula <ericwathome007@gmail.com>
2021-10-07 02:18:51 +0300

    Added login and signup activities

--- app/src/main/java/com/moringaschool/helpdesk/LauncherActivity.java
+++ app/src/main/java/com/moringaschool/helpdesk/LauncherActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.helpdesk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LauncherActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity implements View.OnClickListener{
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        mButton = (Button) findViewById(R.id.get_started);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mButton){
+            startActivity(new Intent(LauncherActivity.this, SignupActivity.class));
+        }
     }
 }
