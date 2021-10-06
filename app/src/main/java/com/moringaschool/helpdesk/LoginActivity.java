acc2221f76e7d527cd0069b8f23393f203af3f32
ericwafula <ericwathome007@gmail.com>
2021-10-07 02:25:53 +0300

    Added navigation between login and signup

--- app/src/main/java/com/moringaschool/helpdesk/LoginActivity.java
+++ app/src/main/java/com/moringaschool/helpdesk/LoginActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.helpdesk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mTextView = (TextView) findViewById(R.id.no_account);
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mTextView){
+            startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+        }
     }
 }
--- app/src/main/java/com/moringaschool/helpdesk/SignupActivity.java
+++ app/src/main/java/com/moringaschool/helpdesk/SignupActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.helpdesk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mTextView = (TextView) findViewById(R.id.have_an_account);
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mTextView){
+            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+        }
     }
 }
