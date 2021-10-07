b9e696840ff6494434ca3c965ef127d830500ef3
ericwafula <ericwathome007@gmail.com>
2021-10-07 17:11:00 +0300

    Added dynamic content to nav view

--- app/src/main/java/com/moringaschool/helpdesk/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/helpdesk/ui/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseUser currentUser;
+
     TextView nav_name;
 
     DrawerLayout drawerLayout;
@@ -35,24 +37,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // sets the user's display name
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                View header = navigationView.getHeaderView(0);
-                nav_name = header.findViewById(R.id.user_name);
-
-                if (user != null) {
-                    nav_name.setText(user.getDisplayName());
-                } else {
-
-                }
-            }
-        };
-
         // <------------ Hooks ------------>
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -60,6 +44,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         // <------------ Toolbar ------------>
         setSupportActionBar(toolbar);
+
+        // ini
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         // <------------ Navigation Drawer Menu ------------>
         navigationView.bringToFront();
@@ -70,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
 
         navigationView.setCheckedItem(R.id.nav_home);
+
+        updateNavHeader();
     }
 
     @Override
@@ -96,12 +86,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-
+    // logs out the user
     private void logout() {
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
+    }
+
+    // sets the user's display name
+    public void updateNavHeader(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        TextView navUsername = headerView.findViewById(R.id.user_name);
+        navUsername.setText(currentUser.getDisplayName());
     }
 }
