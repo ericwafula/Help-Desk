e2deb73903b808d8fcb9013ba62c785330438ed4
ericwafula <ericwathome007@gmail.com>
2021-10-07 09:57:58 +0300

    Added Signup progressbar

--- app/src/main/java/com/moringaschool/helpdesk/ui/SignupActivity.java
+++ app/src/main/java/com/moringaschool/helpdesk/ui/SignupActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +36,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     EditText mPassword;
     EditText mConfirmPassword;
     Button mSignupButton;
+    ProgressBar mSignInProgressBar;
+    TextView mLoadingSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         mPassword = (EditText) findViewById(R.id.editTexTextPassword);
         mConfirmPassword = (EditText) findViewById(R.id.editTexTextConfirmPassword);
         mSignupButton = (Button) findViewById(R.id.signup);
+        mSignInProgressBar = (ProgressBar) findViewById(R.id.firebaseProgressBar);
+        mLoadingSignUp = (TextView) findViewById(R.id.loadingTextView);
 
         mTextView = (TextView) findViewById(R.id.have_an_account);
         mTextView.setOnClickListener(this);
@@ -68,16 +73,37 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    private void showProgressBar() {
+        mSignInProgressBar.setVisibility(View.VISIBLE);
+        mLoadingSignUp.setVisibility(View.VISIBLE);
+        mLoadingSignUp.setText("Sign Up process in Progress");
+    }
+
+    private void hideProgressBar() {
+        mSignInProgressBar.setVisibility(View.GONE);
+        mLoadingSignUp.setVisibility(View.GONE);
+    }
+
     public void createNewUser(){
         final String name = mName.getText().toString().trim();
         final String email = mEmail.getText().toString().trim();
         String password = mPassword.getText().toString().trim();
         String confirmPassword = mConfirmPassword.getText().toString().trim();
 
+        boolean validEmail = isValidEmail(email);
+        boolean validName = isValidName(name);
+        boolean validPassword = isValidPassword(password, confirmPassword);
+        if (!validEmail || !validName || !validPassword) return;
+
+        showProgressBar();
+
         mAuth.createUserWithEmailAndPassword(email, password)
         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+
+                hideProgressBar();
+
                 if (task.isSuccessful()){
                     Toast.makeText(SignupActivity.this, "Authentication successful", Toast.LENGTH_LONG).show();
                 } else {
@@ -116,5 +142,34 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         if (mAuthListener != null){
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEmail.setError("Please enter a valid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidName(String name) {
+        if (name.equals("")) {
+            mName.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mPassword.setError("Please create a password containing at least 6 characters");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mPassword.setError("Passwords do not match");
+            return false;
+        }
+        return true;
     }
 }
